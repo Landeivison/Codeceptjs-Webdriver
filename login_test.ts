8086a95933c36bd010ec9ea3bf964c4a7850e263
Landeivison <lanrhema@gmail.com>
2024-05-09 13:57:43 -0300

    add faker.js

--- login_test.ts
+++ login_test.ts
@@ -1,33 +1,19 @@
+var faker = require('faker');
 Feature('login');
 
 Scenario('Create New Account', ({ I }) => {
 
     I.amOnPage('/')
     I.click('Cadastro')
-    I.fillField('#user', 'testelan3')
-    I.fillField('#email', 'lan3@gmail.com')
+    I.fillField('#user', faker.name.firstName())
+    I.fillField('#email', faker.internet.email())
     I.fillField('#password', '12345678')
     I.click('#btnRegister')
-});
-
-Scenario('Login', ({ I }) => {
-
-    I.amOnPage('/')
-    I.click('Login')
-    I.fillField('#user', 'lan3@gmail.com')
-    I.fillField('#password', '12345678')
-    I.checkOption('#materialUnchecked')
-    I.wait(2)
-    I.click('#btnLogin')
-
-    I.see('Login realizado')
+    I.see('Cadastro realizado!')
     I.wait(3)
     I.click('OK')
-    I.see('lan3@gmail.com')
+    I.see('DASHBOARD')
     I.wait(3)
-
-
-
 });
 
 
